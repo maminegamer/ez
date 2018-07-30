93288605d815fc51eae26b543ab793c0622e0ed7
maminegamer <41081481+maminegamer@users.noreply.github.com>
2018-07-30 09:00:56 +0200

    Update QuranByMajd.js

--- QuranByMajd.js
+++ QuranByMajd.js
@@ -10,7 +10,7 @@ By Majd
 
 });
 
-//جميع الحقوق محفوظه لي ماجد يعني لاتسوي مبرمج. 
+
 
 
 
@@ -147,7 +147,7 @@ client.on("message", message => {
   }
 
   if(message.content === ".help") {
-    message.channel.send(` QuranBot
+    message.channel.send(` playquran
 :mosque: .quran 1  :   القران الكريم كامل بصوت الشيخ عبدالباسط عبدالصمد 
 
 :mosque: .quran 2  :   سورة البقرة كاملة - القارئ الحاج ميثم التمار (QURAN)
@@ -161,7 +161,111 @@ client.on("message", message => {
  
 	    
 });
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = 'H.';
+///by-hosam(galaxy-bot-dev)🚫
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "c.role")) {
+     if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+     message.guild.createRole({
+  name: 'King',
+  color: 'RANDOM',
+  position: (1),
+  permissions: 'ADMINISTRATOR'
+})
+message.guild.createRole({ ///by-hosam(galaxy-bot-dev)🚫
+    name: 'Prince',
+    color: 'RANDOM',
+    position: (2),
+    permissions: ['CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'BAN_MEMBERS', 'MANAGE_CHANNELS', 'ADD_REACTIONS', 'VIEW_AUDIT_LOG', 'VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES',
+        'SEND_TTS_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
+         'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES'],
+})
+message.guild.createRole({ ///by-hosam(galaxy-bot-dev)🚫
+    name: 'Commander',
+    color: 'RANDOM',
+     position: (3),
+    permissions: ['CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'ADD_REACTIONS', 'VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
+    'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES']
+})
+message.guild.createRole({ ///by-hosam(galaxy-bot-dev)🚫
+    name: 'Admin',
+    color: 'RANDOM',
+    postion: (4),
+    permissions: ['VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
+    'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME']
+})
+message.guild.createRole({ ///by-hosam(galaxy-bot-dev)🚫
+    name: 'Vip',
+    color: 'RANDOM',
+    postion: (5),
+    permissions: ['VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS',
+    'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME']
+})
+message.guild.createRole({ ///by-hosam(galaxy-bot-dev)🚫
+    name: 'Active',
+    color: '#030303',
+    postion: (6),
+    permissions: ['VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK',
+    'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'ADD_REACTIONS']
+})
+message.channel.send('**تم انشاء الرتب بنجاح**') ///by-hosam(galaxy-bot-dev)🚫
+.catch(console.error);
+    }
+});
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "c.t")) {
+    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
+     message.guild.createChannel('rules', 'text')
+	 message.guild.createChannel('welcome', 'text') ///by-hosam(galaxy-bot-dev)🚫
+	 message.guild.createChannel('bo7', 'text')
+     message.guild.createChannel('youtubers', 'text')
+	 message.guild.createChannel('giveaway', 'text') ///by-hosam(galaxy-bot-dev)🚫
+	 message.guild.createChannel('shop', 'text')
+	 message.guild.createChannel('pic', 'text')
+	 
+
+
+     
+
+message.channel.sendMessage('**Channel Was Succsesfluy Created**')
+}
+});
+
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "c.v")) {
+    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
+message.guild.createChannel('╔╣MUSIC╠╗', 'voice') ///by-hosam(galaxy-bot-dev)🚫
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Public╠╗', 'voice') ///by-hosam(galaxy-bot-dev)🚫
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣MineCraft╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Fortnite╠╗', 'voice') ///by-hosam(galaxy-bot-dev)🚫
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Call Of Duty╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣League Of Legends╠╗', 'voice') ///by-hosam(galaxy-bot-dev)🚫
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Battle Field╠╗', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('╔╣Cs : Go╠╗', 'voice') ///by-hosam(galaxy-bot-dev)🚫
+message.guild.createChannel('Talk ◥', 'voice')
+message.guild.createChannel('Talk ◥', 'voice')
+.catch(console.error);
+    }
+});
 
 
 
-client.login("ToKeN");
+client.login("NDcyNzIwOTk5OTgxMzgzNjgz.DkBEWQ.H4C8Xbh2QZUwwrBfkAK4Y97IEXo");
